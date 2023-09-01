5e8b9bbf3790cf59e39dd4cf710f5f1d2f0bed0b
Sajidhgn <sajidhgn@gmail.com>
2023-09-01 09:36:53 +0500

    swagger items

--- lib/todolist_web/controllers/item_controller.ex
+++ lib/todolist_web/controllers/item_controller.ex
@@ -1,8 +1,8 @@
 defmodule TodoListWeb.ItemController do
   use TodoListWeb, :controller
 
+  use PhoenixSwagger
   alias TodoList.Repo
-
   alias TodoList.Items
   alias TodoList.Items.Item
 
@@ -14,8 +14,17 @@ defmodule TodoListWeb.ItemController do
     render(conn, :index, items: items)
   end
 
-  def create(conn, %{"item" => item_params}) do
-    IO.inspect(item_params)
+   # Swagger List Function
+   swagger_path :index do
+    get("/api/items")
+    summary("Get total items")
+    description("Get total items")
+    produces("application/json")
+    # security([%{Bearer: []}])
+    response(200, "Ok", Schema.ref(:TotalItems))
+  end
+
+  def create(conn, item_params) do
 
     with {:ok, %Item{} = item} <- Items.create_item(item_params) do
       conn
@@ -23,6 +32,20 @@ defmodule TodoListWeb.ItemController do
       |> put_resp_header("location", ~p"/api/items/#{item}")
       |> render(:show, item: item, message: "Item created successfully")
     end
+  end
+
+  # Swagger Create Function
+  swagger_path :create do
+    post("/api/items")
+    summary("items creation")
+    description("items creation")
+    produces("application/json")
+    # security([%{Bearer: []}])
+
+    parameters do
+      body(:body, Schema.ref(:CreateItem), "items Params", required: true)
+    end
+    response(200, "Ok")
   end
 
   def show(conn, %{"id" => id}) do
@@ -56,4 +79,85 @@ defmodule TodoListWeb.ItemController do
 
   defp list_archived?(%{list: %{archived: true}}), do: true
   defp list_archived?(_), do: false
+
+  def swagger_definitions do
+    %{
+      TotalItems:
+      swagger_schema do
+        title("Items")
+        description("Items details")
+
+        example(%{})
+      end,
+      CreateItem:
+        swagger_schema do
+          title("List Schema")
+          description("List Schema")
+
+          properties do
+              list_id(:boolean, "list_id", required: true)
+              completed(:boolean, "completed", required: true)
+            content(:string, "content", required: true)
+          end
+          example(%{
+            list_id: "sdfgdfgzdfgsdfgsdfgsdfg",
+            completed: true,
+            content: "dummy content",
+
+          })
+        end,
+      #   SingleList:
+      # swagger_schema do
+      #   title("Single List")
+      #   description("List details")
+
+      #   example(%{})
+      # end,
+      # StatusUpdateList:
+      # swagger_schema do
+      #   title("Status update List")
+      #   description("Status update list details")
+
+      #   properties do
+      #     id(:string, "id", required: true)
+      #   title(:string, "title", required: true)
+      #   archived(:boolean, "archived", required: false)
+      # end
+
+      #   example(%{
+      #     archived: true,
+      #           title: "najams",
+      #           id: "4c27de4f-ea2b-44bc-a871-b6f477b7417d"
+      #   })
+      # end,
+      # UpdateList:
+      # swagger_schema do
+      #   title("update List")
+      #   description("update list details")
+
+      #   properties do
+      #     id(:string, "id", required: true)
+      #   title(:string, "title", required: true)
+      #   archived(:boolean, "archived", required: true)
+      # end
+      #   example(%{
+      #           archived: true,
+      #           title: "james",
+      #           id: "4c27de4f-ea2b-44bc-a871-b6f477b7417d"
+
+      #   })
+      # end,
+      # DeleteList:
+      # swagger_schema do
+      #   title("Delete List")
+      #   description("Delete list details")
+
+      #   properties do
+      #     id(:string, "id", required: true)
+      # end
+
+      #   example(%{})
+      # end,
+    }
+  end
 end
